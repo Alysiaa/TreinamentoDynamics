e97c68e85ccca8eab7688a2c97b46cc3c1040095
Alysiaa <carol_lopees@outlook.com>
2023-01-08 12:10:53 -0300

    1º Teste

--- Treinamento Dynamics/Treinamento Dynamics/Conexao.cs
+++ Treinamento Dynamics/Treinamento Dynamics/Conexao.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Xrm.Tooling.Connector;
 
 namespace Treinamento_Dynamics
 {
-    internal class Conexao
+    public class Conexao
     {
+        private static CrmServiceClient crmServiceClientTrinamento;
+
+        public CrmServiceClient ObterConexao()
+        {
+            var connectionStringCRM = @"AuthType=OAuth;
+                             Username = TreinamentoDyn@dyn789.onmicrosoft.com;
+                             Password = 61966196Carol; SkipDiscovery = True;
+                             AppID= 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                             RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                             Url = https://orgb80c66bc.crm2.dynamics.com/main.aspx;";
+
+            if (crmServiceClientTrinamento== null) 
+            {
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                crmServiceClientTrinamento= new CrmServiceClient(connectionStringCRM);
+                    }
+            return crmServiceClientTrinamento;
+        }
     }
 }
--- Treinamento Dynamics/Treinamento Dynamics/Program.cs
+++ Treinamento Dynamics/Treinamento Dynamics/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
 
 namespace Treinamento_Dynamics
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var crmService = new Conexao().ObterConexao();
+            FetchXML(crmService);
+        }
+        #region FetchXML1
+        static void FetchXML(CrmServiceClient crmService)
+        {
+            string query = @"<fetch version= '1.0' output-format= 'xml-platform' mapping= 'logical' distinct= 'false'>
+                             <entity name = 'account'>
+                             <attribute name= 'name'/>
+                             <attribute name='primarycontactid'/>
+                             <attribute name='telephone1'/>
+                             <attribute name='accountid'/>
+                             <attribute name='createdon'/>
+                             <order attribute = 'name' descending = 'false'/>
+                             </entity>
+                             </fetch>";
+
+            EntityCollection colecao = crmService.RetrieveMultiple(new FetchExpression(query));
+            foreach (var item in colecao.Entities)
+            #endregion
+            { }
         }
     }
+   
 }
